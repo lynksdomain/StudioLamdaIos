4498fc7469fca791e9e5c255747a59fc8f0c94c3
Antonio Flores <johndoe@johns-mbp.home>
2020-12-25 18:40:57 -0500

    Setting up Xcode Project and working on the Detail Page info

---
+++ Studio Lamda/Studio Lamda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studio Lamda
+//
+//  Created by Antonio Flores on 12/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
