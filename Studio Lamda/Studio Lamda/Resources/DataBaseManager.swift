c54846617597d752091adcb75cb56de3a34ed870
Antonio Flores <johndoe@johns-mbp.home>
2021-01-25 02:07:18 -0500

    Login feature is working with cocoapods, Pod file was big to add all togther. Fixed signIn bug

--- Studio Lamda/Studio Lamda/Resources/DataBaseManager.swift
+++ Studio Lamda/Studio Lamda/Resources/DataBaseManager.swift
@@ -6,38 +6,38 @@
 //
 
 import Foundation
-//import FirebaseDatabase
+import FirebaseDatabase
 
 final class DatabaseManager {
     
     static let shared = DatabaseManager()
     
-//    private let database = Database.database().reference()
+    private let database = Database.database().reference()
     
     
 }
 
 extension DatabaseManager {
     
-//    public func isUserExists(with email:String, completion: @escaping((Bool) -> Void)) {
-//        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
-//        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
-//
-//        database.child(safeEmail).observeSingleEvent(of: .value) { (snapshot) in
-//            guard snapshot.value as? String != nil else {
-//                completion(false)
-//                return
-//            }
-//            completion(true)
-//        }
-//    }
-//
-//    public func insertUser(user: ChatAppUser){
-//        database.child(user.safeEmail).setValue([
-//                                                    "full_Name":user.fullName,
-//                                                    "progressBar_Updater": user.progressBarUpdater,
-//        ])
-//    }
+    public func isUserExists(with email:String, completion: @escaping((Bool) -> Void)) {
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+
+        database.child(safeEmail).observeSingleEvent(of: .value) { (snapshot) in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        }
+    }
+
+    public func insertUser(user: ChatAppUser){
+        database.child(user.safeEmail).setValue([
+                                                    "full_Name":user.fullName,
+                                                    "progressBar_Updater": user.progressBarUpdater,
+        ])
+    }
 }
 
 
--- Studio Lamda/Studio Lamda/ViewControllers/SignUpViewController.swift
+++ Studio Lamda/Studio Lamda/ViewControllers/SignUpViewController.swift
@@ -15,13 +15,9 @@ class SignUpViewController: UIViewController {
     
     private let spinner = JGProgressHUD(style: .dark)
     
+    var canSignup = false
     
-    
-//    var ref: DatabaseReference!
-
-    
-//    let firebaseAuth = FirebaseAuth
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -45,54 +41,64 @@ class SignUpViewController: UIViewController {
 
         if signUpView.emailTF.text == "" || signUpView.passwordTF.text == "" {
             alertUser(title: "Email Error", message: "Make sure email is fillout properly")
+            self.canSignup = false
+
             return
         }
         
         
         guard let email = signUpView.emailTF.text, let password = signUpView.passwordTF.text, let name = signUpView.nameTF.text, !email.isEmpty, !password.isEmpty, !name.isEmpty  else {
             alertUser(title:"Forum Error", message: "Make sure everthing is filled out coorectly")
+            self.canSignup = false
+
             return
                 }
         
-//        DatabaseManager.shared.isUserExists(with: email) { (exists) in
-//            guard  !exists else {
-//                //USER already exists
-//                
-//                 return
-//            }
-//            
-//            Auth.auth().createUser(withEmail: email, password: password) { [weak self] (result, error) in
-//                guard result != nil, error == nil else {
-//                    print("Error Creating User")
-//                    self?.alertUser(title: "User Error", message: "Error Creating User")
-//                    return
-//                }
-//                self!.spinner.show(in: self!.view)
-//
-//                            
-//                let initialProgressBarUpdater = 0
-//                
-//                let chatUser = ChatAppUser(fullName: name, emailAddress: email, progressBarUpdater: initialProgressBarUpdater)
-//                DatabaseManager.shared.insertUser(user: chatUser)
-//                let user = result?.user
-//                
-//                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-//
-//                self!.spinner.indicatorView = JGProgressHUDSuccessIndicatorView()
-//                }
-//
-//            }
-//        }
-        
-       
-        spinner.dismiss()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
-        
-        self.dismiss(animated: true, completion: nil)
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let mainScreen = storyboard.instantiateViewController(withIdentifier: "mainScreen")
-        self.navigationController?.pushViewController(mainScreen, animated: true)
+        DatabaseManager.shared.isUserExists(with: email) { (exists) in
+            guard  !exists else {
+                //USER already exists
+                self.canSignup = false
+                 return
+            }
+
+            
+            Auth.auth().createUser(withEmail: email, password: password) { [weak self] (result, error) in
+                
+                guard result != nil, error == nil else {
+                    print("Error Creating User")
+                    self?.alertUser(title: " Error", message: "Error Creating User Try Again")
+                    self!.canSignup = false
+                    return
+                }
+                
+                if result != nil  {
+                    self!.canSignup = true
+                }
+                
+                
+                if self!.canSignup {
+                    self!.spinner.show(in: self!.view)
+                    let initialProgressBarUpdater = 0
+                    let chatUser = ChatAppUser(fullName: name, emailAddress: email, progressBarUpdater: initialProgressBarUpdater)
+                    DatabaseManager.shared.insertUser(user: chatUser)
+                    let user = result?.user
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+                    self!.spinner.indicatorView = JGProgressHUDSuccessIndicatorView()
+                    }
+                    self!.spinner.dismiss()
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+                    
+                        self!.dismiss(animated: true, completion: nil)
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let mainScreen = storyboard.instantiateViewController(withIdentifier: "mainScreen")
+                        self?.navigationController?.pushViewController(mainScreen, animated: true)
+                    }
+                }
+                
+                
+
+            }
         }
     }
     
